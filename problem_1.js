8bdbc2c1aad6983f73e2fe1b87b5a8a135305628
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-02-17 19:29:49 +0600

    first commit

---
+++ problem_1.js
@@ -0,0 +1,10 @@
+function calculateVAT(price){
+    if(typeof price!=='number' || price<0){
+        return 'Invalid';
+    }
+    const rate=(price*7.5)/100;
+    return rate;
+}
+const price=1500;
+const result=calculateVAT(price)
+console.log(result)
